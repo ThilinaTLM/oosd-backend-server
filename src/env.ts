9167f4ae3ad55551017eb36f96eeb6437d3a93ec
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2020-05-17 08:41:00 +0530

    Remove ESLint. Add dotenv environment variables. Add .vscode/settings. Add user.http test file.

---
+++ src/env.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import { ConnectionProfile } from "./model/index";
+
+dotenv.config();
+
+export const PORT: number = Number(process.env.SERVE_PORT);
+
+export const MYSQL_PROFILE: ConnectionProfile = {
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT || 3306),
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "",
+    database: process.env.DB_DBASE || "test"
+};
