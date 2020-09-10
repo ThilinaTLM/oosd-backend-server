e20a74c3481265874b798668edbf90a8960eed23
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2020-09-10 09:03:25 +0530

    add a new error type

--- src/database/core/errors.ts
+++ src/database/core/errors.ts
@@ -3,9 +3,10 @@ export interface ModelError extends String {}
 export const ModelErrorSet = {
     NO_ERRORS: "",
     UNKNOWN: "Unknown error",
-    ESSENTIALS_ARE_NULL: "Essential data fields are not provided",
+    ESSENTIALS_ARE_NULL: "Essential database fields are not provided",
     CONNECTION_FAILED: "Cannot connect with database",
     WRONG_ARGUMENTS: "Incorrect arguments",
-    NO_ENTRY_FOUND: "No entry found"
+    NO_ENTRY_FOUND: "No entry found",
+    DUPLICATE: "Duplicate entry"
 }
 
