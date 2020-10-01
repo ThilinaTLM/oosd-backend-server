d8d497b701960f4c59e0544adfdd2a208414cb68
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2020-10-01 13:36:49 +0530

    fix auth issue in utils

--- src/routes/api/customer.ts
+++ src/routes/api/customer.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { customer } from "../../controller/";
+import { preBuilt } from "../../middlewares";
 
 export const cusRouter = Router()
 
-cusRouter.post('/add', customer.add)
+cusRouter.post('/add', preBuilt.ALL_ROLES, customer.add)
 cusRouter.get('/get-customer', customer.get) // query allowed, (customerId, nic, divisionalOffice)
-cusRouter.put('/update-customer/:customerId', customer.update)
+cusRouter.put('/update-customer/:customerId', preBuilt.ALL_ROLES, customer.update)
--- src/routes/api/utils.ts
+++ src/routes/api/utils.ts
@@ -10,5 +10,5 @@ export const utilsRouter = Router();
 utilsRouter.post("/add-div", preBuilt.ONLY_ADMIN, util.addDivision);
 utilsRouter.post("/add-gn-office", preBuilt.ONLY_ADMIN, util.addGNOffice);
 
-utilsRouter.get("/all-divs", preBuilt.ALL_ROLES, util.getAllDivisions);
-utilsRouter.get("/all-gn-offices", preBuilt.ALL_ROLES, util.getAllGNOffices);
+utilsRouter.get("/all-divs", util.getAllDivisions);
+utilsRouter.get("/all-gn-offices", util.getAllGNOffices);
