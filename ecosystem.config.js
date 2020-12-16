695dbe2756b4eb99d242d50a24138ed1a407eda5
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2020-12-16 11:19:09 +0530

    add pm2 config file 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: "oosd-backend-api",
+            name: "api",
             script: "./dist/main.js",
             watch: false,
             env_development: {
