65b269515dcbfced2fd29355b51e1fbd0fa24cf6
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2020-12-16 11:12:23 +0530

    add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps: [
+        {
+            name: "oosd-backend-api",
+            script: "./dist/main.js",
+            watch: false,
+            env_development: {
+                "NODE_ENV": "development",
+                "MYSQL_HOST": "ec2-100-25-45-111.compute-1.amazonaws.com",
+                "MYSQL_PORT": "3306",
+                "MYSQL_USER": "root",
+                "MYSQL_USER_PASSWORD": "root",
+                "MYSQL_DATABASE": "cms",
+                "MYSQL_CONNECTION_LIMIT": 9,
+
+                "SALT_ROUNDS": 10,
+                "SECRET_KEY": "nqygkOnkrQtHJg47Bhdzg7YtvM",
+                "UPLOADS_DIR": "uploads",
+                "PORT": 8000,
+                "ALLOWED_HOST": "all"
+            }
+        }
+    ]
+};
